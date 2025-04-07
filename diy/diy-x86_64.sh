0ff475d26a68c6f6e49913cba1e5398006373d51
OPPEN321 <zj18139624826@gmail.com>
2025-04-07 22:43:38 +0800

    Update diy-x86_64.sh

--- diy/diy-x86_64.sh
+++ diy/diy-x86_64.sh
@@ -36,8 +36,8 @@ src/gz openwrt_luci https://mirrors.aliyun.com/openwrt/releases/24.10.0/packages
 src/gz openwrt_packages https://mirrors.aliyun.com/openwrt/releases/24.10.0/packages/x86_64/packages
 src/gz openwrt_routing https://mirrors.aliyun.com/openwrt/releases/24.10.0/packages/x86_64/routing
 src/gz openwrt_telephony https://mirrors.aliyun.com/openwrt/releases/24.10.0/packages/x86_64/telephony
-src/gz openwrt_core https://raw.githubusercontent.com/oppen321/openwrt-kmod/main/x86_64/v24.10.0
-src/gz openwrt_extras https://raw.githubusercontent.com/oppen321/openwrt-kmod/main/x86_64/package
+src/gz openwrt_core https://raw.githubusercontent.com/oppen321/openwrt_core/main/x86_64/v24.10.0
+src/gz openwrt_extras https://raw.githubusercontent.com/oppen321/openwrt_extras/main/x86_64/package
 EOF
 
 # samba4
